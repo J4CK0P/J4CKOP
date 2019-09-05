239d70440e795944a97e884656553ed336b570bb
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-09-05 15:30:36 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,44 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v("   
+sleep 1
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+clear
+echo
+cd $HOME/J4CKOP
+sh Start.sh
